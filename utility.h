bd026c818e15c33feae2c7c711bd368c9dec774d
keptan <keptan@protonmail.com>
2019-11-05 14:28:10 -1000

    utility methods

---
+++ utility.h
@@ -0,0 +1,22 @@
+#pragma once 
+#include  <random>
+#include  <iterator>
+#include <experimental/random>
+int randInt (int low, int high)
+{
+	return std::experimental::randint(low, high);
+}
+
+template<typename Iter, typename RandomGenerator>
+Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
+    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
+}
+
+template<typename Iter>
+Iter select_randomly(Iter start, Iter end) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return select_randomly(start, end, gen);
+}
